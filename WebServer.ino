06317efed645f377eccd6c4905dbf79daa04b15d
Mathieu Darse <mattdarse@gmail.com>
2013-03-17 22:11:02 +0100

    Re-add display logic (need wiring)

--- WebServer.ino
+++ WebServer.ino
@@ -7,6 +7,7 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <WebServer.h>
+#include "ASCII.h"
 
 // MAC Address of the device
 static byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x50, 0x92 };
@@ -16,7 +17,44 @@ static IPAddress ip(192, 168, 1, 30); // Static IPv4 address
 #define PREFIX ""
 WebServer webserver(PREFIX, 80);
 
-//// ROM-based messages (we only have 1k of RAM we need to be conservative)
+
+//////////////////////
+// Display settings //
+//////////////////////
+#define SIZE(x)  (sizeof(x) / sizeof(x[0]))
+const int charHeight = 5;
+const int charWidth = 5;
+// Define pin setup
+const int coilPin = 2; // Coil driver
+const int ratePin = A0;
+const int ledPins[] = {3,4,5,6,7,8}; // LEDs
+const int ledPinsSize = 6;
+// Init coil vars
+int coilState = LOW;
+long coilLastUpdate = 0;
+long coilTickInterval = 100000;
+// Init display vars
+char    displayString[8]     = "";
+int     displayStringLength    = 0;
+int     displayCharacterIndex  = 0;
+int     displayCharacterColumn = 0;
+boolean displayNeedsSpace      = false;
+boolean displayReverse         = false;
+long    displayLastRefresh     = 0;
+long    displayRefreshInterval = 100000;
+// Geometry
+int displayWidth       = 30; // Display horizontal resolution
+int displayStringWidth = 0;
+// Init text vars
+String inputString = "";        // a string to hold incoming data
+boolean stringComplete = false; // whether the string is complete
+
+
+
+////////////////////////
+// ROM-based messages (we only have 1k of RAM we need to be conservative)
+////////////////////////
+
 P(Page_start) = "<!DOCTYPE html>\n<html><head><title>TrainsPOVing</title></head><body>\n";
 P(Page_end) = "</body></html>";
 P(Hello_world) = "<h1>Hello POV !</h1>\n<img src=\"http://i.imgur.com/f6V29.gif\">\n<br>\n<a href=\"display\">manage display</a>";
@@ -35,7 +73,10 @@ P(Params_end) = "End of parameters<br>\n";
 P(Post_params_begin) = "Parameters sent by POST:<br>\n";
 
 
-//// Start of commands
+
+//////////////
+// Commands //
+//////////////
 
 void helloCommand(WebServer &server, WebServer::ConnectionType type, char * url_tail, bool tail_complete) {
   server.httpSuccess(); // HTTP 200
@@ -45,7 +86,6 @@ void helloCommand(WebServer &server, WebServer::ConnectionType type, char * url_
   server.printP(Hello_world);
   server.printP(Page_end);
 }
-
 
 #define NAMELEN 32
 #define VALUELEN 32
@@ -137,13 +177,19 @@ void displayCommand(WebServer &server, WebServer::ConnectionType type, char *, b
   server.printP(Page_end);
 }
 
-//// End of commands
 
+
+///////////////////
+// Arduino stuff //
+///////////////////
 
 void setup() {
-   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-
+  // Pin setup
+  pinMode(coilPin, OUTPUT);
+  for (int pin = 0; pin < ledPinsSize; pin++) {
+    pinMode(ledPins[pin], OUTPUT);
+  }
+  
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   webserver.setDefaultCommand(&helloCommand);
@@ -151,6 +197,8 @@ void setup() {
   webserver.addCommand("diag.html", &parseCommand);
   webserver.begin();
   
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
 }
@@ -161,5 +209,109 @@ void loop() {
   
   // process incoming connections (one at a time)
   webserver.processConnection(buffer, &length);
+  
+  // print the string when a newline arrives) 
+  if (stringComplete) {
+    displayCharacterIndex  = 0;
+    displayCharacterColumn = 0;
+    displayNeedsSpace      = false;
+    displayReverse         = false;
+    inputString.toCharArray(displayString, sizeof(displayString));
+    displayStringLength = inputString.length();
+    // width = letter number x letter width + a space between each letter
+    displayStringWidth = displayStringLength * (charWidth + 1) - 1;
+    Serial.println("Received \"" + inputString + "\"");
+    Serial.println(inputString.length());
+    // clear the string
+    inputString = "";
+    stringComplete = false;
+  }
+  
+    
+  unsigned long now = micros();
+  
+  // Coil actuator
+  if (now - coilLastUpdate > coilTickInterval) {
+    coilLastUpdate = now; // update last tick
+    // It's time to move the coil
+    if (coilState == LOW) {
+      coilState = HIGH;
+      displayCharacterIndex  = 0;
+      displayCharacterColumn = 0;
+      displayNeedsSpace      = false;
+      displayReverse         = false;
+    }
+    else {
+      coilState = LOW;
+      displayReverse = true;
+    }
+    digitalWrite(coilPin, coilState);
+    
+    // Rate
+    int sensorValue = analogRead(ratePin);
+    coilTickInterval = map(sensorValue, 0, 1023, 10000, 1000000);
+    displayRefreshInterval = coilTickInterval / displayStringWidth;
+  }
+
+  // Update LEDs
+  if (now - displayLastRefresh > displayRefreshInterval) {
+    displayLastRefresh = now;
+    if (displayNeedsSpace) {
+       // Serial.println("-----");
+      printColumn(B00000, charHeight);
+      displayNeedsSpace = false;
+    }
+    else if (displayReverse)
+      printColumn(B00000, charHeight);
+    else
+      printNextColumn();
+  }
+}
+
+void printNextColumn() {
+  // Show next column
+  char character = displayString[displayCharacterIndex];
+  if (character == '\0') { // If end of string
+    displayCharacterIndex = 0;
+    displayCharacterColumn = 0;
+    character = displayString[0];
+  }
+  byte column = ASCII[character - 0x20][displayCharacterColumn];
+  // Serial.print(displayCharacterIndex);
+  // Serial.print(':');
+  // Serial.print(displayCharacterColumn);
+  // Serial.print('=');
+  // Serial.println(column, BIN);
+  printColumn(column, charHeight);
+  
+  // Move markers to next column
+  if (displayCharacterColumn == charWidth - 1) { // If end of a char
+    displayCharacterIndex++;
+    displayCharacterColumn = 0;
+    displayNeedsSpace = true; // Mark next loop for inserting a thin space
+  }
+  else {
+    displayCharacterColumn++;
+  }
+}
+
+void printColumn(byte column, int size) {
+  for (int y=0; y<size; y++) {
+    // Get current pixel
+    boolean pixel = column & (1 << y);
+    digitalWrite(ledPins[y], pixel);
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') { // if the incoming character is a newline
+      stringComplete = true;
+    }
+    else {
+      inputString += inChar;
+    }
+  }
 }
 
