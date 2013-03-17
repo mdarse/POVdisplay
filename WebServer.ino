d201628e7e6ad806acd8be390ce55715b69fa5ba
Mathieu Darse <mattdarse@gmail.com>
2013-03-17 20:32:55 +0100

    Add a cool hello world

--- WebServer.ino
+++ WebServer.ino
@@ -19,12 +19,12 @@ WebServer webserver(PREFIX, 80);
 //// ROM-based messages (we only have 1k of RAM we need to be conservative)
 P(Page_start) = "<!DOCTYPE html>\n<html><head><title>TrainsPOVing</title></head><body>\n";
 P(Page_end) = "</body></html>";
+P(Hello_world) = "<h1>Hello POV !</h1>\n<img src=\"http://i.imgur.com/f6V29.gif\">";
+
 P(Get_head) = "<h1>GET from ";
 P(Post_head) = "<h1>POST to ";
 P(Unknown_head) = "<h1>UNKNOWN request for ";
-P(Default_head) = "unidentified URL requested.</h1><br>\n";
-P(Raw_head) = "raw.html requested.</h1><br>\n";
-P(Parsed_head) = "parsed.html requested.</h1><br>\n";
+P(Parsed_head) = "diag.html requested.</h1><br>\n";
 P(Good_tail_begin) = "URL tail = '";
 P(Bad_tail_begin) = "INCOMPLETE URL tail = '";
 P(Tail_end) = "'<br>\n";
@@ -32,33 +32,16 @@ P(Parsed_tail_begin) = "URL parameters:<br>\n";
 P(Parsed_item_separator) = " = '";
 P(Params_end) = "End of parameters<br>\n";
 P(Post_params_begin) = "Parameters sent by POST:<br>\n";
-P(Line_break) = "<br>\n";
 
 
 //// Start of commands
 
 void helloCommand(WebServer &server, WebServer::ConnectionType type, char * url_tail, bool tail_complete) {
-  // HTTP OK
-  server.httpSuccess();
-
+  server.httpSuccess(); // HTTP 200
   if (type == WebServer::HEAD) return;
-  
+  // Print a Hello World
   server.printP(Page_start);
-  switch (type) {
-    case WebServer::GET:
-      server.printP(Get_head);
-      break;
-    case WebServer::POST:
-      server.printP(Post_head);
-      break;
-    default:
-      server.printP(Unknown_head);
-  }
-
-  server.printP(Default_head);
-  server.printP(tail_complete ? Good_tail_begin : Bad_tail_begin);
-  server.print(url_tail);
-  server.printP(Tail_end);
+  server.printP(Hello_world);
   server.printP(Page_end);
 }
 
@@ -129,7 +112,6 @@ void setup() {
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   webserver.setDefaultCommand(&helloCommand);
-  webserver.addCommand("index.html", &helloCommand); // alias for / (defaultCommand)
   webserver.addCommand("diag.html", &parseCommand);
   webserver.begin();
   
